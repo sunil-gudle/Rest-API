916e9b3348b888039e4a509668f6f0f8951e40b0
Sunil Gudle <sunilgudle7844@gmail.com>
2023-02-07 10:58:13 +0530

    Basic Operations

---
+++ src/main/java/Spring/Boot/L1/cms/dao/CustomerDAO.java
@@ -0,0 +1,14 @@
+package Spring.Boot.L1.cms.dao;
+
+import Spring.Boot.L1.cms.model.Customer;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface CustomerDAO extends CrudRepository<Customer, Integer> {
+
+    @Override
+    List<Customer> findAll();
+}
