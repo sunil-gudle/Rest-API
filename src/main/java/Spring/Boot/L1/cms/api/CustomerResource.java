f71deb7615a2330db9ba6be7f99b2b7ef66f0ab3
Sunil Gudle <sunilgudle7844@gmail.com>
2023-02-17 12:45:18 +0530

    Customized changes and Exception handling and added application security

--- src/main/java/Spring/Boot/L1/cms/api/CustomerResource.java
+++ src/main/java/Spring/Boot/L1/cms/api/CustomerResource.java
@@ -15,24 +15,28 @@ public class CustomerResource {
     private CustomerService customerService;
 
     @PostMapping
-    public Customer addCustomer(@RequestBody Customer customer){
+    public Customer addCustomer(@RequestBody Customer customer) {
         return customerService.addCustomer(customer);
     }
+
     @GetMapping
-    public List<Customer> getCustomers(){
+    public List<Customer> getCustomers() {
         return customerService.getCustomers();
     }
+
     @GetMapping(value = "/{customerId}")
-    public Customer getCustomer(@PathVariable("customerId") int customerId){
+    public Customer getCustomer(@PathVariable("customerId") int customerId) {
         return customerService.getCustomer(customerId);
     }
+
     @PutMapping(value = "/{customerId}")
-    public Customer updateCustomer(@PathVariable("customerId") int customerId, @RequestBody Customer customer){
+    public Customer updateCustomer(@PathVariable("customerId") int customerId, @RequestBody Customer customer) {
 
         return customerService.updateCustomer(customerId, customer);
     }
+
     @DeleteMapping(value = "/{customerId}")
-    public void deleteCustomer(@PathVariable("customerId") int customerId){
+    public void deleteCustomer(@PathVariable("customerId") int customerId) {
         customerService.deleteCustomer(customerId);
     }
 
